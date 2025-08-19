575687ddc1c4bb615f907f1dbb3d4deb2c0840fc
Gokul Nath K <Gokul Nath K@LAPTOP-I9A1QEHT>
2025-08-19 13:53:48 +0530

    additon

---
+++ Java_core_programs/src/javaprogram/Add.java
@@ -0,0 +1,13 @@
+package javaprogram;
+
+public class Add {
+	public static void main(String[] args) {
+        int num1 = 10;   // first number
+        int num2 = 20;   // second number
+
+        int sum = num1 + num2;  // addition
+
+        System.out.println("The sum is: " + sum);
+    }
+
+}
