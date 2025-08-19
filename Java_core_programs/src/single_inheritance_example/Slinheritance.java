e2fcefe02f973667bedba71a9b4c9e652406c551
Gokul Nath K <Gokul Nath K@LAPTOP-I9A1QEHT>
2025-08-19 16:30:44 +0530

    SingInheritance

---
+++ Java_core_programs/src/single_inheritance_example/Slinheritance.java
@@ -0,0 +1,14 @@
+package single_inheritance_example;
+
+public class Slinheritance {
+			public static void main(String[] args) {
+				Trainer t =new Trainer( 006,"Vengat", "pondy", 1234, "new", "chennai");
+				System.out.println(t);
+
+			}
+
+		
+
+	}
+
+
