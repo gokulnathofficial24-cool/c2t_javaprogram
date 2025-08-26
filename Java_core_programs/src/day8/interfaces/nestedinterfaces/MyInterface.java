99209cfdd2747b7c344d135722efccc5fe6b24d7
Gokul Nath K <Gokul Nath K@LAPTOP-I9A1QEHT>
2025-08-26 10:42:51 +0530

    new

---
+++ Java_core_programs/src/day8/interfaces/nestedinterfaces/MyInterface.java
@@ -0,0 +1,13 @@
+package day8.interfaces.nestedinterfaces;
+
+public interface MyInterface {
+	
+	void calculateArea(){
+		interface MyInnerInterface {
+		       int  id = 20;
+		       void print();
+		
+	}
+
+}
+}
